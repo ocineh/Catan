dc1e5ca80c502cc78242d2b5bf3e7ca95cf2ebfc
Hocine <ocineh@hotmail.com>
2021-12-30 08:55:08 +0100

    Add TrayCellView class

--- src/main/java/catan/views/TrayView.java
+++ src/main/java/catan/views/TrayView.java
@@ -8,11 +8,35 @@ import java.awt.*;
 import java.util.List;
 
 public class TrayView extends JPanel {
+    private final Tray tray;
+
     public TrayView(Tray tray) {
+        this.tray = tray;
         setLayout(new GridLayout(tray.getHeight(), tray.getWidth()));
         setBorder(new EmptyBorder(5, 5, 5, 5));
-        List<TileView> tileViews = tray.stream().map(cell -> new TileView(cell.getTile())).toList();
-        for(TileView cell : tileViews) add(cell);
+        List<TrayCellView> tileViews = tray.stream().map(TrayCellView::new).toList();
+        for(TrayCellView cell : tileViews) add(cell);
         setPreferredSize(new Dimension(tray.getWidth() * 100, tray.getHeight() * 100));
+    }
+
+    public static class TrayCellView extends JPanel {
+        private final Tray.TrayCell cell;
+        private final TileView tileView;
+
+        public TrayCellView(Tray.TrayCell cell) {
+            this.cell = cell;
+            this.tileView = new TileView(cell.getTile());
+
+            setLayout(new BorderLayout());
+            add(tileView, BorderLayout.CENTER);
+        }
+
+        public TileView getTileView() {
+            return tileView;
+        }
+
+        public Tray.TrayCell getCell() {
+            return cell;
+        }
     }
 }
