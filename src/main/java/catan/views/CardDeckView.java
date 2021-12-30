44e2f9b046bcec705a7bd668c0cad5c54b0d9d0d
Hocine <ocineh@hotmail.com>
2021-12-30 08:43:46 +0100

    Move the CardsView class to the CardDeckView class

--- src/main/java/catan/views/CardDeckView.java
+++ src/main/java/catan/views/CardDeckView.java
@@ -4,9 +4,12 @@ import catan.models.Card;
 import catan.models.CardDeck;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.util.LinkedList;
+import java.util.function.ToIntFunction;
 
 public class CardDeckView extends JPanel {
     private final LinkedList<CardsView> cardsViews;
@@ -29,5 +32,26 @@ public class CardDeckView extends JPanel {
 
     public void update(CardDeck cardDeck) {
         cardsViews.forEach(cardsView -> cardsView.update(cardDeck));
+    }
+
+    public static class CardsView extends JPanel {
+        private final JLabel label;
+        private final String cardName;
+        private final ToIntFunction<CardDeck> function;
+
+        public CardsView(String cardName, ToIntFunction<CardDeck> function) {
+            this.cardName = cardName;
+            this.function = function;
+            Border border = new LineBorder(Color.BLACK, 2, true);
+            setBorder(border);
+
+            label = new JLabel(cardName + ": 0");
+            label.setBorder(new EmptyBorder(1, 1, 1, 1));
+            add(label);
+        }
+
+        public void update(CardDeck cardDeck) {
+            label.setText(cardName + ": " + function.applyAsInt(cardDeck));
+        }
     }
 }
